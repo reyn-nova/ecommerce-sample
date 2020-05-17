f9ce16379fa14c49bce55e349f0946e763caac9e
rp-nova <reynaldpn@gmail.com>
2020-05-18 01:08:34 +0700

    Updates

---
+++ precache-manifest.a26a6b499381ccffc0e887da4cc7cfa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a348c0a7b3aad9ab57012a46a574ec3",
+    "url": "/ecommerce-sample/index.html"
+  },
+  {
+    "revision": "63dc684042a11a328ab6",
+    "url": "/ecommerce-sample/static/css/main.e9edb719.chunk.css"
+  },
+  {
+    "revision": "ed59622cecf3590c6dd4",
+    "url": "/ecommerce-sample/static/js/2.459fb51e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ecommerce-sample/static/js/2.459fb51e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63dc684042a11a328ab6",
+    "url": "/ecommerce-sample/static/js/main.3248e8e6.chunk.js"
+  },
+  {
+    "revision": "91a584f27d2dc092aeab",
+    "url": "/ecommerce-sample/static/js/runtime-main.53b9183c.js"
+  }
+]);
